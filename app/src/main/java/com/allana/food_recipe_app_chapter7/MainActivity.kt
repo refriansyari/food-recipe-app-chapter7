2cceacc06d3947d503533b2034480029f39d25c8
Maulana Hanif A.R <maulanahanif33@gmail.com>
2022-03-18 08:37:09 +0700

    Initial project

---
+++ app/src/main/java/com/allana/food_recipe_app_chapter7/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.allana.food_recipe_app_chapter7
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
