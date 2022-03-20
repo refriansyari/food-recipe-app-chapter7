82924e729597ce698812c9434a256f1d7bee0024
ikhsanhdyt <ikhsnhdyt@gmail.com>
2022-03-20 20:58:24 +0700

    Create layout xml register page

---
+++ app/src/main/java/com/allana/food_recipe_app_chapter7/ui/register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.allana.food_recipe_app_chapter7.ui.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.allana.food_recipe_app_chapter7.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
