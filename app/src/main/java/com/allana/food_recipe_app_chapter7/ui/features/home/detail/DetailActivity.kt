e7eac2ccfd530d9fee8e045cd7df4383df74d1e7
sekarayusavitri <savitrisekar10@gmail.com>
2022-03-21 08:20:49 +0700

    [sekar] check point home

--- app/src/main/java/com/allana/food_recipe_app_chapter7/ui/features/home/detail/DetailActivity.kt
+++ app/src/main/java/com/allana/food_recipe_app_chapter7/ui/features/home/detail/DetailActivity.kt
@@ -1,12 +1,12 @@
-package com.allana.food_recipe_app_chapter7.ui.home
+package com.allana.food_recipe_app_chapter7.ui.features.home.detail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.allana.food_recipe_app_chapter7.R
 
-class HomeActivity : AppCompatActivity() {
+class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_detail)
     }
 }
