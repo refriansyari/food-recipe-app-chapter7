2fd48f9c6402c416aee08c3f90a16e73a9e54cef
thomasaja <thomas.wendra@gmail.com>
2022-03-24 17:48:14 +0700

    add dependency injection loginpage, data source

--- app/src/main/java/com/allana/food_recipe_app_chapter7/di/RepositoryModule.kt
+++ app/src/main/java/com/allana/food_recipe_app_chapter7/di/RepositoryModule.kt
@@ -1,7 +1,10 @@
 package com.allana.food_recipe_app_chapter7.di
 
+import com.allana.food_recipe_app_chapter7.data.local.preference.datasource.LocalDataAuthSource
 import com.allana.food_recipe_app_chapter7.data.network.datasource.RecipeDataSource
+import com.allana.food_recipe_app_chapter7.data.network.datasource.auth.AuthApiDataSource
 import com.allana.food_recipe_app_chapter7.ui.features.home.HomeRepository
+import com.allana.food_recipe_app_chapter7.ui.loginpage.LoginPageRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -19,6 +22,15 @@ object RepositoryModule {
     ): HomeRepository {
         // TODO add localDataSource
         return HomeRepository(recipeDataSource)
+    }
+
+    @Provides
+    @Singleton
+    fun provideLoginPageRepository(
+        authApiDataSource: AuthApiDataSource,
+        localDataAuthSource: LocalDataAuthSource
+    ) : LoginPageRepository{
+        return LoginPageRepository(authApiDataSource,localDataAuthSource)
     }
 
     // TODO add detailRecipeRepo
--- app/src/main/java/com/allana/food_recipe_app_chapter7/ui/loginpage/LoginPageRepository.kt
+++ app/src/main/java/com/allana/food_recipe_app_chapter7/ui/loginpage/LoginPageRepository.kt
@@ -1,4 +1,24 @@
 package com.allana.food_recipe_app_chapter7.ui.loginpage
 
-class LoginPageRepository {
+import com.allana.food_recipe_app_chapter7.base.arch.BaseRepositoryImpl
+import com.allana.food_recipe_app_chapter7.data.local.preference.datasource.LocalDataAuthSource
+import com.allana.food_recipe_app_chapter7.data.network.datasource.auth.AuthApiDataSource
+import com.allana.food_recipe_app_chapter7.data.network.model.request.AuthRequest
+import com.allana.food_recipe_app_chapter7.data.network.model.response.auth.BaseAuthResponse
+import com.allana.food_recipe_app_chapter7.data.network.model.response.auth.User
+import javax.inject.Inject
+
+class LoginPageRepository @Inject constructor(
+    private val authApiDataSource : AuthApiDataSource,
+    private val localDataAuthSource: LocalDataAuthSource
+) : BaseRepositoryImpl(), LoginPageContract.Repository{
+    override suspend fun postLoginUser(loginRequest: AuthRequest): BaseAuthResponse<User, String> {
+        val loginResponse = authApiDataSource.postLoginUser(loginRequest)
+        if (loginResponse.issuccess){
+            localDataAuthSource.setAuthToken(loginResponse.data.token)
+            val userResponse = authApiDataSource.getProfileData()
+            localDataAuthSource.saveUserData(userResponse.data)
+        }
+        return loginResponse
+    }
 }
