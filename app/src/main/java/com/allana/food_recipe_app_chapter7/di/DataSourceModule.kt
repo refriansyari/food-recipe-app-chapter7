0a7a757364e6b0a1294d11b9e2cd5946ec6f9b7d
muhsinulfikri <mohpikry90@gmail.com>
2022-03-26 20:07:04 +0700

    finishing inject database to hilt

--- app/src/main/java/com/allana/food_recipe_app_chapter7/di/DataSourceModule.kt
+++ app/src/main/java/com/allana/food_recipe_app_chapter7/di/DataSourceModule.kt
@@ -1,16 +1,17 @@
 package com.allana.food_recipe_app_chapter7.di
 
+import android.content.Context
 import com.allana.food_recipe_app_chapter7.data.local.room.FavRecipeDatabase
 import com.allana.food_recipe_app_chapter7.data.local.room.dao.FavoriteRecipeDao
 import com.allana.food_recipe_app_chapter7.data.local.room.datasource.FavoriteRecipeDataSource
 import com.allana.food_recipe_app_chapter7.data.local.room.datasource.FavoriteRecipeDataSourceImpl
-import com.allana.food_recipe_app_chapter7.data.local.room.entity.FavoriteRecipe
 import com.allana.food_recipe_app_chapter7.data.network.datasource.RecipeDataSource
 import com.allana.food_recipe_app_chapter7.data.network.datasource.RecipeDataSourceImpl
 import com.allana.food_recipe_app_chapter7.data.network.services.RecipeApiService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -29,5 +30,16 @@ object DataSourceModule {
     @Provides
     fun provideFavoriteRecipeDataSource(favRecipeDatabase: FavoriteRecipeDao): FavoriteRecipeDataSource{
         return FavoriteRecipeDataSourceImpl(favRecipeDatabase)
+    }
+
+    @Singleton
+    @Provides
+    fun providesFavoriteRecipeDao(favRecipeDatabase: FavRecipeDatabase): FavoriteRecipeDao{
+        return favRecipeDatabase.favoriteRecipeDao()
+    }
+    @Singleton
+    @Provides
+    fun provideRecipeFavoriteDatabase(@ApplicationContext appContext: Context): FavRecipeDatabase{
+        return FavRecipeDatabase.getInstance(appContext)
     }
 }
