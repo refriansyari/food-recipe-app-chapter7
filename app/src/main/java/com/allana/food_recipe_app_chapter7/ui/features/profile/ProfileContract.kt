2f0b9ac81073b3d31efc6eb65aea9b4ea20acf0b
JordJ19 <jordie.juwono96@gmail.com>
2022-03-27 14:23:25 +0700

    create base fragment for profile

---
+++ app/src/main/java/com/allana/food_recipe_app_chapter7/ui/features/profile/ProfileContract.kt
@@ -0,0 +1,15 @@
+package com.allana.food_recipe_app_chapter7.ui.features.profile
+
+import com.allana.food_recipe_app_chapter7.base.arch.BaseContract
+
+interface ProfileContract {
+    interface View : BaseContract.BaseView {
+
+    }
+    interface ViewModel : BaseContract.BaseViewModel {
+
+    }
+    interface Repository : BaseContract.BaseRepository {
+
+    }
+}
---
+++ app/src/main/java/com/allana/food_recipe_app_chapter7/ui/features/profile/ProfileViewModel.kt
@@ -0,0 +1,7 @@
+package com.allana.food_recipe_app_chapter7.ui.features.profile
+
+import com.allana.food_recipe_app_chapter7.base.arch.BaseContract
+import com.allana.food_recipe_app_chapter7.base.arch.BaseViewModelImpl
+
+class ProfileViewModel : BaseContract.BaseViewModel, BaseViewModelImpl() {
+}
