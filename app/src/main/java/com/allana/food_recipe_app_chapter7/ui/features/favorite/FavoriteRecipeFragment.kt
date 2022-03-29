d91f877727679c56c2d2dcf2aabcc2b8ecc6e668
Maulana Hanif A.R <maulanahanif33@gmail.com>
2022-03-29 19:52:43 +0700

    update favorite recipe

--- app/src/main/java/com/allana/food_recipe_app_chapter7/ui/features/favorite/FavoriteRecipeFragment.kt
+++ app/src/main/java/com/allana/food_recipe_app_chapter7/ui/features/favorite/FavoriteRecipeFragment.kt
@@ -22,7 +22,11 @@ FavoriteRecipeContract.View {
 
     override fun initView() {
         getViewBinding().etSearchFavoriteRecipe.addTextChangedListener{ searchQuery ->
-            getSearchData(searchQuery.toString())
+            searchQuery?.let {
+                if (searchQuery.toString().isNotEmpty()) {
+                    getSearchData(searchQuery.toString())
+                }
+            }
         }
         initList()
         getListData()
