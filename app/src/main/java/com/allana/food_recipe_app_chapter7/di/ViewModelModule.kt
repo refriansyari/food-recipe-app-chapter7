5d15714c9517f3242b548bf7ef01b256be5ef7f0
sekar <sekar.savitri@bankmandiri.co.id>
2022-03-29 23:12:24 +0700

    [UPDATE] upadte ViewModelModule

--- app/src/main/java/com/allana/food_recipe_app_chapter7/di/ViewModelModule.kt
+++ app/src/main/java/com/allana/food_recipe_app_chapter7/di/ViewModelModule.kt
@@ -9,8 +9,6 @@ import com.allana.food_recipe_app_chapter7.ui.features.profile.ProfileRepository
 import com.allana.food_recipe_app_chapter7.ui.features.profile.ProfileViewModel
 import com.allana.food_recipe_app_chapter7.ui.features.home.detail.DetailRepository
 import com.allana.food_recipe_app_chapter7.ui.features.home.detail.DetailViewModel
-import com.allana.food_recipe_app_chapter7.ui.features.profile.ProfileRepository
-import com.allana.food_recipe_app_chapter7.ui.features.profile.ProfileViewModel
 import com.allana.food_recipe_app_chapter7.ui.loginpage.LoginPageRepository
 import com.allana.food_recipe_app_chapter7.ui.loginpage.LoginPageViewModel
 import com.allana.food_recipe_app_chapter7.ui.register.RegisterRepository
