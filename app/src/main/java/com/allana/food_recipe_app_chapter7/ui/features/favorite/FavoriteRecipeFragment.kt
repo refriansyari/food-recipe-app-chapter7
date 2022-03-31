7d884d94c125a214d77ccc2c0c2f6751150880de
Maulana Hanif A.R <maulanahanif33@gmail.com>
2022-03-31 13:46:59 +0700

    optimize code swipe delete

--- app/src/main/java/com/allana/food_recipe_app_chapter7/ui/features/favorite/FavoriteRecipeFragment.kt
+++ app/src/main/java/com/allana/food_recipe_app_chapter7/ui/features/favorite/FavoriteRecipeFragment.kt
@@ -23,6 +23,7 @@ class FavoriteRecipeFragment : BaseFragment<FragmentFavoriteRecipeBinding, Favor
 FavoriteRecipeContract.View {
 
     private lateinit var adapter: FavoriteRecipeAdapter
+    private var listFavRecipe: List<FavoriteRecipe>? = null
 
     override fun initView() {
         getViewBinding().etSearchFavoriteRecipe.addTextChangedListener{ searchQuery ->
@@ -42,6 +43,31 @@ FavoriteRecipeContract.View {
         getViewBinding().rvFavoriteRecipe.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = this@FavoriteRecipeFragment.adapter
+        }
+
+        val itemTouchHelperCallback = object: ItemTouchHelper.SimpleCallback(
+            ItemTouchHelper.UP or ItemTouchHelper.DOWN,
+            ItemTouchHelper.RIGHT or ItemTouchHelper.LEFT
+        ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                val favRecipe = listFavRecipe?.get(position)
+                favRecipe?.let { getViewModel().deleteFavoriteRecipe(it) }
+                Snackbar.make(requireView(), getString(R.string.success_deleted_fav_recipe), Snackbar.LENGTH_SHORT).show()
+                getListData()
+            }
+        }
+
+        ItemTouchHelper(itemTouchHelperCallback).apply {
+            attachToRecyclerView(getViewBinding().rvFavoriteRecipe)
         }
 
     }
@@ -83,31 +109,7 @@ FavoriteRecipeContract.View {
     private fun setDataAdapter(data: List<FavoriteRecipe>?) {
         data?.let {
             adapter.setItems(it)
-
-            val itemTouchHelperCallback = object: ItemTouchHelper.SimpleCallback(
-                ItemTouchHelper.UP or ItemTouchHelper.DOWN,
-                ItemTouchHelper.RIGHT or ItemTouchHelper.LEFT
-            ) {
-                override fun onMove(
-                    recyclerView: RecyclerView,
-                    viewHolder: RecyclerView.ViewHolder,
-                    target: RecyclerView.ViewHolder
-                ): Boolean {
-                    return true
-                }
-
-                override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-                    val position = viewHolder.adapterPosition
-                    val favRecipe = it[position]
-                    getViewModel().deleteFavoriteRecipe(favRecipe)
-                    Snackbar.make(requireView(), getString(R.string.success_deleted_fav_recipe), Snackbar.LENGTH_SHORT).show()
-                    getListData()
-                }
-            }
-
-            ItemTouchHelper(itemTouchHelperCallback).apply {
-                attachToRecyclerView(getViewBinding().rvFavoriteRecipe)
-            }
+            listFavRecipe = it
         }
     }
 
